cf7827e085f34131fffa7bb07e374d1272abe758
leandrucarvalho <leandrucs@gmail.com>
2022-03-11 22:14:32 -0300

    subindo projeto

---
+++ js/setup.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$(".open").on("click", function() {
+    $(".popup-overlay, .popup-content").addClass("active");
+  });
+  
+  //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+  $(".close, .popup-overlay").on("click", function() {
+    $(".popup-overlay, .popup-content").removeClass("active");
+  });
